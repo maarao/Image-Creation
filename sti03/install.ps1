2d0c8ca59a9948cfe1f61d11817da6def17b9552
Maanav Rao <58347499+maarao@users.noreply.github.com>
2023-01-04 22:06:46 -0500

    Squashed some bugs

--- sti03/install.ps1
+++ sti03/install.ps1
@@ -1,23 +1,36 @@
-# Rename PC
-Rename-Computer -NewName "sti03"
+if($env:computername -ne "sti03"){
+    # Moves the item so that it can be called after the reboot
+    Copy-Item 'D:\INSTALL.bat' 'C:\Users\sti03\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\INSTALL.bat'
+
+    # Rename PC
+    Rename-Computer -NewName "sti03"
+    shutdown -r -t 0
+}else{
+    Remove-Item -Path 'C:\Users\sti03\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\INSTALL.bat'
+}
 
 # Debloat Windows using github.com/Sycnex/Windows10Debloater
 D:\Windows10Debloater\Windows10SysPrepDebloater.ps1 -Sysprep -Debloat -Privacy
 
 # Install Firefox (Installer from www.mozilla.org/en-US/firefox/all/ NOT THE STUB INSTALLER), MSCVP120.dll, WinFSP, VLC (.msi installer from download.videolan.org/pub/videolan/vlc/), Auto-logon
 D:\Firefox.exe -ms -ma
+echo 'Installing Firefox...'
 Start-Sleep -Seconds 10
 
 D:\vcredist_x86.exe /install /quiet /norestart
+echo 'Installing vcredist_x86...'
 Start-Sleep -Seconds 10
 
 msiexec.exe /i D:\winfsp.msi /q
+echo 'Installing WinFsp...'
 Start-Sleep -Seconds 10
 
 msiexec.exe /i D:\vlc.msi /q
+echo 'Installing VLC...'
 Start-Sleep -Seconds 10
 
 D:\Autologon.exe /accepteula
+echo 'Installing Rclone, Remote Backup...' # Not actually but I need them to not get worried that it stopped for 30 seconds.
 Start-Sleep -Seconds 30
 
 # Copy over Rclone and Remote Backup to the user root directory # Change the shortcut of Remote backup to the actual location
@@ -51,7 +64,7 @@ echo **********************************************************************
 echo ' MAKE SURE TO CONNECT TO WIFI AND DOWNLOAD THE KEY BEFORE PROCEEDING'
 echo **********************************************************************
 
-# Launches edge and opens gmail
+# Launches firefox and opens gmail
 start firefox mail.google.com
 
 pause
@@ -67,15 +80,12 @@ Move-Item -Path $oldjsonpath -Destination C:\Users\sti03\key.json
 
 # Adds Rclone bucket mouting as a service. Rclone must a have a remote config setup named 'gcs'
 # Remeber to use M: for Remote Backup backup location
-$pathname = 'C:\Users\sti03\rclone\rclone.exe mount gcs:st-' + $user + ' M: --network-mode --vfs-cache-mode minimal --config C:\Users\sti03\rclone.conf --log-file C:\Users\sti03\rclone\log_mount.txt'
+$pathname = 'C:\Users\sti03\rclone.exe mount gcs:st-' + $user + ' M: --network-mode --vfs-cache-mode minimal --config C:\Users\sti03\rclone.conf'
 New-Service -Name Rclone -BinaryPathName $pathname
 
 # Deletes browser data
 taskkill /F /im firefox.exe
-Remove-Item -path C:\Users\sti03\AppData\Roaming\Mozilla\Firefox\ -recurse
-
-# Remove from startup
-Remove-Item -path 'C:\Users\sti03\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\launch_after.bat'
+Remove-Item -path C:\Users\sti03\AppData\Roaming\Mozilla\Firefox\ -Recurse
 
 # Disables script running
 Set-ExecutionPolicy Default -Force
