6a4c773f969c73cf42d9cd4d2bda86c2666b7700
Maanav Rao <58347499+maarao@users.noreply.github.com>
2023-01-30 23:56:15 -0500

    Made the camera recording separated between systems

--- sti05/Setup.sh
+++ sti05/Setup.sh
@@ -11,7 +11,7 @@ printf "[Unit]\nDescription=Motion service\n\n[Service]\nExecStart=/usr/bin/moti
 # Setup base motion.conf
 sudo rm /etc/motion/motion.conf
 sudo touch /etc/motion/motion.conf
-printf "daemon off\nsetup_mode off\nlog_level 6\nemulate_motion off\nthreshold 1500\ndespeckle_filter EedDl\nminimum_motion_frames 1\nevent_gap 60\npre_capture 3\npost_capture 0\npicture_output off\nmovie_output on\nmovie_max_time 900\nmovie_quality 0\nmovie_codec mkv\nwebcontrol_parms 0\nstream_port 0\nwidth 1280\nheight 720\nframerate 15\ntarget_dir /backup/\nmovie_filename %%\$/%%Y-%%m-%%d/%%T" | sudo tee -a /etc/motion/motion.conf
+printf "daemon off\nsetup_mode off\nlog_level 6\nemulate_motion off\nthreshold 1500\ndespeckle_filter EedDl\nminimum_motion_frames 1\nevent_gap 60\npre_capture 3\npost_capture 0\npicture_output off\nmovie_output on\nmovie_max_time 900\nmovie_quality 0\nmovie_codec mkv\nwebcontrol_parms 0\nstream_port 0\nwidth 1280\nheight 720\nframerate 15\nmovie_filename %%\$/%%Y-%%m-%%d/%%T" | sudo tee -a /etc/motion/motion.conf
 
 printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
@@ -53,13 +53,13 @@ do
         # LTS - Note the port change
         if [ $vrtype -eq "1" ]
         then
-            printf "\nnetcam_url rtsp://${username}:${password}@${vrip}:8554/streaming/channels/${camera}02" | sudo tee -a /etc/motion/System-$i/Camera-$camera.conf
+            printf "\nnetcam_url rtsp://${username}:${password}@${vrip}:8554/streaming/channels/${camera}02\ntarget_dir /backup/System-$i" | sudo tee -a /etc/motion/System-$i/Camera-$camera.conf
         fi
 
         # LOREX
         if [ $vrtype -eq "2" ]
         then
-            printf "\nnetcam_url rtsp://${username}:${password}@${vrip}:554/cam/realmonitor?channel=${camera}&subtype=0" | sudo tee -a /etc/motion/System-$i/Camera-$camera.conf
+            printf "\nnetcam_url rtsp://${username}:${password}@${vrip}:554/cam/realmonitor?channel=${camera}&subtype=0\ntarget_dir /backup/System-$i" | sudo tee -a /etc/motion/System-$i/Camera-$camera.conf
         fi
                 
         printf "\ncamera /etc/motion/System-$i/Camera-$camera.conf" | sudo tee -a /etc/motion/motion.conf
